99ea796296c96843b3aaa763638202d893064cb9
Daniel Nijdl <nijdlm@gmail.com>
2020-12-16 15:10:51 -0700

    Fixed a few things in the contract and fixed bugs on a few pages

---
+++ book.js
@@ -0,0 +1,3 @@
+function contractWindow() {
+    let popup = window.open("contract.html", "_blank", "width=800,height=800,top=400,left=400,resizable=yes,toolbar=yes,scrollbar=yes");
+}
--- home.js
+++
@@ -1,3 +0,0 @@
-function contractWindow() {
-    let popup = window.open("contract.html", "_blank", "width=400,height=400,top=400,left=400,resizable=yes,toolbar=yes,scrollbar=yes");
-}
